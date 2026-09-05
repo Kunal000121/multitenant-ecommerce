071b49507d7548904457b4122ca5972036cbaf28
Kunal000121 <bhkunal28@gmail.com>
2026-09-05 10:47:57 +0530

    03:home layout bug fix

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -51,7 +51,7 @@ const navbarItems = [
 
 export const Navbar = () => {
     const pathname = usePathname();
-    const[isSidebarOpen, setIsSidebarOpen] = React.useState(true);
+    const[isSidebarOpen, setIsSidebarOpen] = React.useState(false);
 
     return(
         <nav className="h-20 flex border-b justify-between font-medium bg-white">
